166d9b2ff9fb84bfbcc4225d29b79c6ea4f87cb1
debiabril <debora.badosa@gmail.com>
2023-11-21 10:57:56 -0300

    cambio de mayus

--- src/main/java/org/example/model/entity/Servicio.java
+++ src/main/java/org/example/model/entity/Servicio.java
@@ -12,10 +12,10 @@ public class Servicio {
     @OneToMany(mappedBy = "trabajo", cascade = CascadeType.ALL)
     private List<Trabajo> trabajos;
     @ManyToOne
-    @JoinColumn(name = "id_Admin")
+    @JoinColumn(name = "id_admin")
     private Admin admin;
     @ManyToOne
-    @JoinColumn(name = "id_Prestador")
+    @JoinColumn(name = "id_prestador")
     private Prestador prestador;
     @Id
     @GeneratedValue
